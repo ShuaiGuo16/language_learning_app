15b500bb013faf10db3daeba84d4defe45a41919
Shuai Guo <shuaiguo0916@hotmail.com>
2023-06-23 22:53:01 +0200

    add libraries

---
+++ chatbot.py
@@ -0,0 +1,18 @@
+# Content: Class definition of interactive chatbot system 
+# Author: Shuai Guo
+# Email: shuaiguo0916@hotmail.com
+# Date: June, 2023
+
+import os
+import openai
+from langchain.prompts import (
+    ChatPromptTemplate, 
+    MessagesPlaceholder, 
+    SystemMessagePromptTemplate, 
+    HumanMessagePromptTemplate
+)
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.chains import ConversationChain
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
