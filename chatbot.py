c69829686af3e5d10e8ac942bb9b09ff02179756
Shuai Guo <shuaiguo0916@hotmail.com>
2023-06-23 22:53:40 +0200

    chatbot class constructor

--- chatbot.py
+++ chatbot.py
@@ -16,3 +16,36 @@ from langchain.chains import LLMChain
 from langchain.chains import ConversationChain
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
+
+
+class Chatbot:
+    """Class definition for a single chatbot with memory, created with LangChain."""
+    
+    def __init__(self, engine):
+        """Select backbone large language model, as well as instantiate 
+        the memory for creating language chain in LangChain.
+        
+        Parameters:
+        --------------
+        
+        engine: the backbone llm-based chat model.
+                "OpenAI" stands for OpenAI chat model;
+                Other chat models are also possible in LangChain, 
+                see https://python.langchain.com/en/latest/modules/models/chat/integrations.html
+        """
+        
+        # Instantiate llm
+        if engine == 'OpenAI':
+            # Reminder: need to set up openAI API key 
+            # (e.g., via environment variable OPENAI_API_KEY)
+            self.llm = ChatOpenAI(
+                model_name="gpt-3.5-turbo",
+                temperature=0.7
+            )
+
+
+        else:
+            raise KeyError("Currently unsupported chat model type!")
+        
+        # Instantiate memory
+        self.memory = ConversationBufferMemory(return_messages=True)
