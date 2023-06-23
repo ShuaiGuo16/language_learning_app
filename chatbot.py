4482a0af990d350055d6f4ff319abf6b89005910
Shuai Guo <shuaiguo0916@hotmail.com>
2023-06-23 22:54:05 +0200

    chatbot instruction

--- chatbot.py
+++ chatbot.py
@@ -49,3 +49,52 @@ class Chatbot:
         
         # Instantiate memory
         self.memory = ConversationBufferMemory(return_messages=True)
+
+
+
+    def instruct(self, role, oppo_role, language, scenario, 
+                 session_length, proficiency_level, 
+                 learning_mode, starter=False):
+        """Determine the context of chatbot interaction. 
+        
+        Parameters:
+        -----------
+        
+        role: the role played by the current bot. 
+        oppo_role: the role played by the opponent bot.
+        language: the language the conversation/debate will be conducted. This is 
+                  the target language the user is trying to learn.
+        scenario: for conversation, scenario represents the place where the conversation 
+                  is happening; for debate, scenario represents the debating topic.
+        session_length: the number of exchanges between two chatbots. Two levels are possible:
+                        "Short" or "Long".
+        proficiency_level: assumed user's proficiency level in target language. This 
+                           provides the guideline for the chatbots in terms of the 
+                           language complexity they will use. Three levels are possible:
+                           "Beginner", "Intermediate", and "Advanced".
+        learning_mode: two modes are possible for language learning purposes:
+                       "Conversation" --> where two bots are chatting in a specified scenario;
+                       "Debate" --> where two bots are debating on a specified topic.
+        starter: flag to indicate if the current chatbot should lead the talking.
+        """
+
+        # Define language settings
+        self.role = role
+        self.oppo_role = oppo_role
+        self.language = language
+        self.scenario = scenario
+        self.session_length = session_length
+        self.proficiency_level = proficiency_level
+        self.learning_mode = learning_mode
+        self.starter = starter
+        
+        # Define prompt template
+        prompt = ChatPromptTemplate.from_messages([
+            SystemMessagePromptTemplate.from_template(self._specify_system_message()),
+            MessagesPlaceholder(variable_name="history"),
+            HumanMessagePromptTemplate.from_template("""{input}""")
+        ])
+        
+        # Create conversation chain
+        self.conversation = ConversationChain(memory=self.memory, prompt=prompt, 
+                                              llm=self.llm, verbose=False)
