c60944f9adc8e5f6b49563f1bdf20b782dae80bb
Shuai Guo <shuaiguo0916@hotmail.com>
2023-06-25 00:16:43 +0200

    add logic to avoid showing messages twice

--- app.py
+++ app.py
@@ -195,11 +195,6 @@ if 'dual_chatbots' not in st.session_state:
 
 if 'dual_chatbots' in st.session_state:  
 
-    # Retrieve generated conversation & chatbots
-    mesg1_list = st.session_state.bot1_mesg
-    mesg2_list = st.session_state.bot2_mesg
-    dual_chatbots = st.session_state['dual_chatbots']
-
     # Show translation 
     if translate_col.button('Translate to English'):
         st.session_state['translate_flag'] = True
@@ -214,14 +209,34 @@ if 'dual_chatbots' in st.session_state:
     if audio_col.button('Play audio'):
         st.session_state['audio_flag'] = True
         st.session_state['batch_flag'] = True
+
+    # Retrieve generated conversation & chatbots
+    mesg1_list = st.session_state.bot1_mesg
+    mesg2_list = st.session_state.bot2_mesg
+    dual_chatbots = st.session_state['dual_chatbots']
     
-    # Show complete message
-    show_messages(mesg1_list, mesg2_list, 
-                  container=conversation_container,
-                  time_delay=time_delay,
-                  batch=st.session_state['batch_flag'],
-                  audio=st.session_state['audio_flag'],
-                  translation=st.session_state['translate_flag'])
+    # Control message appear
+    if st.session_state["first_time_exec"]:
+        st.session_state['first_time_exec'] = False
+    
+    else:
+        # Show complete message
+        with conversation_container:
+            
+            if learning_mode == 'Conversation':
+                st.write(f"""#### {role1} and {role2} {scenario} 🎭""")
+
+            else:
+                st.write(f"""#### Debate 💬: {scenario}""")
+        
+            for mesg_1, mesg_2 in zip(mesg1_list, mesg2_list):
+                new_count = show_messages(mesg_1, mesg_2, 
+                                        st.session_state["message_counter"],
+                                        time_delay=time_delay,
+                                        batch=st.session_state['batch_flag'],
+                                        audio=st.session_state['audio_flag'],
+                                        translation=st.session_state['translate_flag'])
+                st.session_state["message_counter"] = new_count
     
 
     # Create summary for key learning points
